7b3c4a01b60cb95b443836991fe65b8a61a4736a
Olia Kvasha <gurzhiy.olia@gmail.com>
2019-01-22 12:49:49 +0200

    task2

--- hw10/script.js
+++ hw10/script.js
@@ -4,6 +4,17 @@ if (a > 10) {
     console.log('This number is more than 10')
 } else {
     console.log('This number is less than 10')
-};
+}
 
-
+/*task2*/
+let lang = 'ru';
+switch (lang) {
+    case 'ru':
+        console.log(['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь']);
+        break;
+    case 'en':
+        console.log(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']);
+        break;
+    default:
+        console.log('Error');
+}

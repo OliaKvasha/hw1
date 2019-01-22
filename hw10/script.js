183d4096dee3e5636c861b5d2acf2f7659039c65
Olia Kvasha <gurzhiy.olia@gmail.com>
2019-01-22 20:25:57 +0200

    task3

--- hw10/script.js
+++ hw10/script.js
@@ -18,3 +18,11 @@ switch (lang) {
     default:
         console.log('Error');
 }
+
+/*task3*/
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i = 2; i < arr.length; i++) {
+    if (i % 2 === 0) {
+        console.log(arr[i])
+    }
+}

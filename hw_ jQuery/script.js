8aaf40db3bd56e54f513d1351455fa95e3a9209f
Olia Kvasha <gurzhiy.olia@gmail.com>
2019-02-17 00:59:55 +0200

    step 3

--- hw_ jQuery/script.js
+++ hw_ jQuery/script.js
@@ -4,10 +4,28 @@
 
     $('#footer').append($('#header nav').clone());
 
+
     // 2. Створити "акордеон"
 
     $(".accordion").on("click", ".accordion-header", function() {
         $(this).toggleClass("active").next().slideToggle();
+    });
+
+
+    // 3. Створити попап
+
+    $(".open").on("click", function(){
+        $(".overlay, .modal").addClass("active");
+    });
+
+    $(".close, .overlay").on("click", function(){
+        $(".overlay, .modal").removeClass("active");
+    });
+
+    $(document).keyup(function(e) {
+        if (e.keyCode === 27) {
+            $(".overlay, .modal").removeClass("active");
+        }
     });
 
 }(jQuery));

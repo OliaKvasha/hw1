a7926eee45315ba702a7b4daac478b5e07fa60c3
Olia Kvasha <gurzhiy.olia@gmail.com>
2019-02-17 00:16:31 +0200

    step 2

--- hw_ jQuery/script.js
+++ hw_ jQuery/script.js
@@ -1,5 +1,13 @@
 (function ($) {
 
+    // 1. Засобами jQuery дублюйте меню в футер сторінки.
+
     $('#footer').append($('#header nav').clone());
+
+    // 2. Створити "акордеон"
+
+    $(".accordion").on("click", ".accordion-header", function() {
+        $(this).toggleClass("active").next().slideToggle();
+    });
 
 }(jQuery));
